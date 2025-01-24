50778d7f013c05375760340d7901b86a430e5efd
pineapplemousse <seahkaya@gmail.com>
2025-01-24 14:25:30 +0800

    Edited Op1

--- Opt1_DisplayAllCurrency.py
+++ Opt1_DisplayAllCurrency.py
@@ -1,6 +1,4 @@
 def opt1():
-    filePath = 'cryptoPortfolio.csv'
-    file = open(filePath)
-    data = file.readlines()
-    print(data)
-    file.close()
+    with open('cryptoPortfolio.csv') as file:
+      data = file.readlines()
+      print(data)
