f6c6e48875073191da4f3912a6f0c2f2f51f4cd0
Lohithroshan45 <lohithroshan.23@ichat.sp.edu.sg>
2025-01-31 23:42:07 +0800

    Update Opt3_AmendCurrency.py

--- Opt3_AmendCurrency.py
+++ Opt3_AmendCurrency.py
@@ -1,14 +1,16 @@
 def opt3():
- print("-----------------------------------------------")
- opt=input("Select an option:")
- print("No - CryptoCurrency")
- print("---------------------------")
- print("0 - Bitcoin\n1 - Etherum\n2 -Solana\n3 - Decentraland\n4 - The Sandbox\n5 - Dodgecoin\n6 - Shiba Inu")
- print("---------------------------")
- selection=input("Enter 0 to 6 for your selection or E to exit : ")
- if selection=="0" :
-readline
- if selection=="E" :
- else:
-   print("Invalid input.")
-   opt3()
+ while True:
+  print("No - CryptoCurrency")
+  print("---------------------------")
+  print("0 - Bitcoin\n1 - Etherum\n2 -Solana\n3 - Decentraland\n4 - The Sandbox\n5 - Dodgecoin\n6 - Shiba Inu")
+  print("---------------------------")
+  selection=input("Enter 0 to 6 for your selection or E to exit : ")
+  
+ 
+
+
+
+
+
+# Call the function
+opt3()
